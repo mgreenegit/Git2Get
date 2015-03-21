c1071799a4dd022b2a7d4654da2427722d6fa9fc
mgreenegit <migreene@outlook.com>
2015-03-21 08:38:35 -0500

    Set NuGet executable path

--- Build.ps1
+++ Build.ps1
@@ -1,4 +1,6 @@
+$nugetExe = if(Test-Path Env:NuGet) { Get-Content env:NuGet } else { Join-Path $solutionFolder ".nuget\nuget.exe" }
+
 foreach ($dscModule in (Get-ChildItem .\ -filter *.psd1 -Recurse | % FullName)) {
     $moduleData = $dscModule | Test-ModuleManifest
-    . $nugetExe pack $moduleData.Name -OutputDirectory .\Packages -NonInteractive -Version $moduleData.version
+    . $nugetExe pack $($moduleData.Name) -OutputDirectory .\Packages -NonInteractive -Version $($moduleData.version)
 }
