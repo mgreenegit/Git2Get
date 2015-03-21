1e31863021ce5248100a9a6d3c5d41a0c293e317
mgreenegit <migreene@outlook.com>
2015-03-21 09:07:42 -0500

    Build script now creates NuSpec

--- Build.ps1
+++ Build.ps1
@@ -1,6 +1,23 @@
-$nugetExe = if(Test-Path Env:NuGet) { Get-Content env:NuGet } else { Join-Path $solutionFolder ".nuget\nuget.exe" }
+$nugetExe = if(Test-Path Env:NuGet) { Get-Content env:NuGet } else { Join-Path .\ "nuget\nuget.exe" }
 
 foreach ($dscModule in (Get-ChildItem .\ -filter *.psd1 -Recurse | % FullName)) {
     $moduleData = $dscModule | Test-ModuleManifest
-    . $nugetExe pack $($moduleData.Name) -OutputDirectory .\Packages -NonInteractive -Version $($moduleData.version)
+    $NuSpec = Join-Path $moduleData.ModuleBase "$moduleData.nuspec"
+@"
+<?xml version="1.0"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd">
+  <metadata>
+    <id>$moduleData</id>
+    <version>$($moduleData.Version)</version>
+    <authors>$($moduleData.Author)</authors>
+    <owners>$($moduleData.CompanyName)</owners>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <description>$($moduleData.Description)</description>
+    <releaseNotes />
+    <copyright>$($moduleData.Copyright)</copyright>
+    <tags>PSModule PSIncludes_DscResource PSDscResource_$moduleData</tags>
+  </metadata>
+</package>
+"@ | Out-File $NuSpec
+    . $nugetExe pack $NuSpec -OutputDirectory .\Packages -NonInteractive -Version $($moduleData.version)
 }
